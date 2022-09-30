a48f561f0aa5018817bfd30110ada86fe0381008
yurii-hupalo <85635193+yurii-hupalo@users.noreply.github.com>
2022-09-30 11:48:18 +0300

    Update and rename file in .js format where you need to use all the operators mentioned in the presentation.js to file1.js

---
+++ file1.js
@@ -0,0 +1,15 @@
+//this is commented code
+/*
+ *this is multiply commented lines
+*/
+//print result
+let a = 2;
+let b = 3;
+console.log(a+b);
+//------------------
+//assignment operators
+const side = a;
+const side1 = b;
+const rectangle = a * b;
+console.log(rectangle);
+//------------------
